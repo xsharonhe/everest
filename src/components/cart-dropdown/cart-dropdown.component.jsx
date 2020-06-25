bec04abae7978ac6d4b8995fc58987c9a110f24f
xsharonhe <xsharonhe@gmail.com>
2020-06-25 13:31:35 -0400

    Add cart dropdown feature component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
         <Button> GO TO CHECKOUT </Button>
     </div>
 )
 
-export default Cart
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(Cart);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,7 @@
 const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem =>
          cartItem.id === cartItemToAdd.id);
+         
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
