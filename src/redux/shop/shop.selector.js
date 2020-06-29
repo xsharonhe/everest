2a654e9c0b8884c15b8002bf5f0ae9b9d9153c6c
xsharonhe <xsharonhe@gmail.com>
2020-06-29 17:59:18 -0400

    Map store ids to route paths

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,24 @@
 import { createSelector } from 'reselect'
 
+const COLLECTION_ID_MAP = {
+    bands: 1,
+    trackers: 2,
+    equipment: 3,
+    sports: 4,
+    apparel: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
+)
+
+export const selectCollection = collectionUrlParam => (
+    createSelector(
+        [selectShopCollections],
+        collections => collections.find(collection =>
+            collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    )
 )
