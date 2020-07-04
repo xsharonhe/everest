68c24f58d4dc72de6399b8d32972c4fc9839ca96
xsharonhe <xsharonhe@gmail.com>
2020-07-04 18:40:53 -0400

    Remove redux-logger middleware from production build after heroku deployment

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -59,7 +59,7 @@ class SignIn extends React.Component {
                     <div className='buttons'>
                         <CustomButton type='submit' value='Submit Form'> Sign In </CustomButton>
                         <CustomButton type='button' onClick={ signInWithGoogle } isGoogleSignIn>  
-                            Sign In With Google  </CustomButton>
+                            Google Sign In  </CustomButton>
                     </div>         
                 </form> 
             </div>
