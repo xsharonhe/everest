232781f80d4f941806249353435a682bddf647b0
xsharonhe <xsharonhe@gmail.com>
2020-07-09 16:17:55 -0400

    Store store data in Firebase

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchtoProps = dispatch => ({
