72e5877c84cab425591a5b6c7cc097a82337fa07
xsharonhe <xsharonhe@gmail.com>
2020-07-15 01:10:15 -0400

    Server changes to show notification

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
 
